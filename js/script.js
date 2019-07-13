f8d8df4adda5535fb2ce20832a0404e7eddfbf1d
matuzalema <matuzalema@gmail.com>
2019-07-13 22:01:48 +0200

    Fix add value and date in js

--- js/script.js
+++ js/script.js
@@ -5,45 +5,52 @@ const button = document.querySelector(".button-converter");
 const plnField = document.querySelector(".pln");
 const dateField = document.querySelector(".date");
 
-
-
-
 button.addEventListener("click", function() {
   let inputValue = input.value;
+
   if (isNaN(inputValue) || inputValue === "") {
-      alert("wpisz liczbę");
-      return input.value = "";
+    alert("wpisz liczbę");
+    return (input.value = "");
   } else {
-      let plnAfterConvert = (inputValue/4.2669).toFixed(4);
-       plnField.innerHTML = inputValue + " euro to " + plnAfterConvert + " pln";
+    let plnAfterConvert = (inputValue / 4.2669).toFixed(4);
+    plnFieldValue = plnAfterConvert + " PLN";
 
-       let currentData = function(){
+    let getTime = new Date();
+    let currentMonth = getTime.getMonth();
+    let currentDay = getTime.getDay();
+    let currentHours = getTime.getHours();
+    let currentMinutes = getTime.getMinutes();
 
-       }
-       let getTime = new Date();
-       let currentMonth = getTime.getMonth();
-      let currentDay = getTime.getDay();
-      let currentHours = getTime.getHours();
-      let currentMinutes = getTime.getMinutes();
+    if (currentMonth < 10) {
+      currentMonth = "0" + currentMonth;
+    }
+    if (currentDay < 10) {
+      currentDay = "0" + currentDay;
+    }
+    if (currentHours < 10) {
+      currentHours = "0" + currentHours;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    let date =
+      getTime.getFullYear() +
+      "." +
+      currentMonth +
+      "." +
+      currentDay +
+      " " +
+      currentHours +
+      ":" +
+      currentMinutes;
 
 
-       if (currentMonth < 10) {
-           currentMonth = "0" + currentMonth;
-       } 
-       if (currentDay < 10) {
-          currentDay = "0" + currentDay;
-       } 
-       if (currentHours < 10) {
-           currentHours = "0" + currentHours;
-       }
-      if (currentMinutes < 10) {
-          currentMinutes = "0" + currentMinutes;
-      } 
-       let date = getTime.getFullYear()+ "."+ currentMonth+"." + currentDay + " " + currentHours + ":" + currentMinutes;
-       
-       dateField.innerHTML = date;
+    plnField.insertAdjacentHTML(
+      "afterbegin",
+      "<div class='value-field'>" + plnFieldValue + "</div> <p class='date-field'>" + date + "</p>"
+    );
 
-       console.log(date);
+    console.log(date);
+    input.value = "";
   }
-  console.log(inputValue);
 });
