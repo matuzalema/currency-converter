fa7da640c400d388552b430690cdd7968724ceed
matuzalema <matuzalema@gmail.com>
2019-07-13 15:40:52 +0200

    Initialize project

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let euro = 4.2669;
+
+const input = document.querySelector(".input-converter");
+const button = document.querySelector(".button-converter");
+const plnField = document.querySelector(".pln");
+const dateField = document.querySelector(".date");
+
+
+
+
+button.addEventListener("click", function() {
+  let inputValue = input.value;
+  if (isNaN(inputValue) || inputValue === "") {
+      alert("wpisz liczbę");
+      return input.value = "";
+  } else {
+      let plnAfterConvert = (inputValue/4.2669).toFixed(4);
+       plnField.innerHTML = inputValue + " euro to " + plnAfterConvert + " pln";
+
+       let currentData = function(){
+
+       }
+       let getTime = new Date();
+       let currentMonth = getTime.getMonth();
+      let currentDay = getTime.getDay();
+      let currentHours = getTime.getHours();
+      let currentMinutes = getTime.getMinutes();
+
+
+       if (currentMonth < 10) {
+           currentMonth = "0" + currentMonth;
+       } 
+       if (currentDay < 10) {
+          currentDay = "0" + currentDay;
+       } 
+       if (currentHours < 10) {
+           currentHours = "0" + currentHours;
+       }
+      if (currentMinutes < 10) {
+          currentMinutes = "0" + currentMinutes;
+      } 
+       let date = getTime.getFullYear()+ "."+ currentMonth+"." + currentDay + " " + currentHours + ":" + currentMinutes;
+       
+       dateField.innerHTML = date;
+
+       console.log(date);
+  }
+  console.log(inputValue);
+});
